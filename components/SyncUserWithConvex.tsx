789958145fe36b04a09ec66d1e87a1111cdff041
Maestro1982 <kris.tote@telenet.be>
2024-11-25 13:22:10 +0100

    implement header with searchbar and syncing users between convex and clerk

---
+++ components/SyncUserWithConvex.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useUser } from '@clerk/nextjs';
+
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+
+export const SyncUserWithConvex = () => {
+  const { user } = useUser();
+  const updateUser = useMutation(api.users.updateUser);
+
+  useEffect(() => {
+    if (!user) return;
+
+    const syncUser = async () => {
+      try {
+        await updateUser({
+          userId: user.id,
+          name: `${user.firstName ?? ''} ${user.lastName ?? ''}`.trim(),
+          email: user.emailAddresses[0].emailAddress ?? '',
+        });
+      } catch (error) {
+        console.error('Error syncing user', error);
+      }
+    };
+    syncUser();
+  }, [user, updateUser]);
+
+  return null;
+};
